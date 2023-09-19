ea90e807c767d2bbb0ffcb08a0c982815cc71005
Yaroslav Trach <zhbr282@gmail.com>
2023-09-18 20:25:57 -0400

    Initial Commit

---
+++ TestLoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestLoginApp
+//
+//  Created by User on 2023-09-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
